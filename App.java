1ff7302b98fe433d3938638fe48b2dc72977e24f
SiddheshMarne <125675273+SiddheshMarne@users.noreply.github.com>
2024-02-18 22:48:51 +0530

    Retrive Data from database

--- App.java
+++ App.java
@@ -1,78 +1,43 @@
 package com.tca;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.util.Scanner;
+import java.sql.ResultSet;
 
 public class App {
 
-	public static void main(String[] args) throws Exception {
-
+	public static void main(String[] args)throws Exception 
+	{
+		// TODO Auto-generated method stub
 		Connection con = null;
 		PreparedStatement ps = null;
+		ResultSet rs = null;
 		String dbURL = "jdbc:mysql://localhost:3306/hfbdb";
 		String user = "root";
 		String password = "root";
-		Scanner sc = new Scanner(System.in);
-
-		try {
+		try 
+		{
 
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			con = DriverManager.getConnection(dbURL, user, password);
-
-			/*
-			 * System.out.println("Enter the Roll Number :"); int rno = sc.nextInt();
-			 * System.out.println("Enter Name"); String name = sc.next();
-			 * System.out.println("Enter the Per : "); double per = sc.nextDouble();
-			 */
-			File f = new File("Student.txt");
-			if (!f.exists()) {
-				System.out.println("File does not exists");
-				System.exit(0);
+			
+			ps = con.prepareStatement("select * from student");
+			rs = ps.executeQuery();
+			
+			System.out.println("RNO\tNAME\tPER");
+			while(rs.next())
+			{
+				System.out.println(rs.getInt("rno") +"\t"+rs.getString("name") +"\t"+rs.getFloat("per"));
 			}
-			if (!f.isFile()) {
-				System.out.println("Not a file");
-				System.exit(0);
-			}
-			FileReader fr = new FileReader(f);
-			BufferedReader br = new BufferedReader(fr);
-			String str = "";
-			int sval = 0;
-			ps = con.prepareStatement("insert into student values(?,?,?)");
-			while (true) {
-				str = br.readLine();
-
-				if (str == null) {
-					break;
-				}
-				String[] arrOfStr = str.split(",");
-				System.out.println(str);
-
-				ps.setInt(1, Integer.parseInt(arrOfStr[0]));
-				ps.setString(2, arrOfStr[1]);
-				ps.setDouble(3, Double.parseDouble(arrOfStr[2]));
-				sval = ps.executeUpdate();
-
-			}
-			fr.close();
-
-			if (sval == 1) {
-				System.out.println("Records added");
-
-			} else {
-				System.out.println("Unable to Insert");
-			}
-
-		} catch (Exception e) {
+			
+		} 
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 		} finally {
 			con.close();
 		}
-
 	}
 
 }
